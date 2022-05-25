f9cc74746262d2543e7c758b8972c9cb90e51041
elboywithgear <77282469+elboywithgear@users.noreply.github.com>
2022-05-25 10:25:49 +0700

    BurnEffect

--- Assets/Scripts/BowScript.cs
+++ Assets/Scripts/BowScript.cs
@@ -30,6 +30,8 @@ public class BowScript : MonoBehaviour
     public bool isUsingBomp;
 
     public Animator animator;
+    public Camera fpsCam;
+    
 
     void Start()
     {
@@ -63,151 +65,217 @@ public class BowScript : MonoBehaviour
             isUsingBomp = true;
         }
 
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+
+        if(Physics.Raycast(ray,out hit)){
+            targetPoint = hit.point;
+
+        }
+        else{
+            targetPoint = ray.GetPoint(75);
+        }
+
+        Vector3 direction = targetPoint - spawn.position;
+
         switch (isUsingBomp)
         {
             case true:
 
                 if(PlayerStats.Money >= 100)
                 {
-                    BompArrow();
+                    //BompArrow();
+
+                    
+
+                    if (Input.GetKey(fireButton) && _charge < chargeMax)
+                    {
+
+                        animator.Play("BowHolding");
+
+                        _charge += Time.deltaTime * chargeRate;
+
+                        bompArrowObjWhileHolding.SetActive(true);
+                    }
+
+                    if(Input.GetKeyUp(fireButton))
+                    {
+
+                        animator.Play("BowRelease");
+                        bompArrowObjWhileHolding.SetActive(false);
+                        GameObject arrow = Instantiate(bompArrowObject, spawn.position, Quaternion.identity);
+                        arrow.transform.forward = direction.normalized;
+                        arrow.GetComponent<Rigidbody>().AddForce(direction.normalized * _charge, ForceMode.Impulse);
+                        _charge = 0;
+
+                    }
                 }
                 break;
 
             case false:
 
-                DefaultArrow();
+                //DefaultArrow();
+
+                if (Input.GetKey(fireButton) && _charge < chargeMax)
+                {
+
+                    animator.Play("BowHolding");
+
+                    _charge += Time.deltaTime * chargeRate;
+
+                    arrowObjWhileHolding.SetActive(true);
+                }
+
+                if(Input.GetKeyUp(fireButton))
+                {
+
+                    animator.Play("BowRelease");
+                    arrowObjWhileHolding.SetActive(false);
+                    GameObject arrow = Instantiate(arrowObject, spawn.position, Quaternion.identity);
+                    arrow.transform.forward = direction.normalized;
+                    arrow.GetComponent<Rigidbody>().AddForce(direction.normalized * _charge, ForceMode.Impulse);
+                    _charge = 0;
+
+                }
+
+                
+
+                
+                
 
                 break;
         }          
     }
   
-    public void DefaultArrow()
-    {
-        if (Input.GetKey(fireButton) && _charge < chargeMax)
-        {
-            animator.Play("BowHolding");
-            _charge += Time.deltaTime * chargeRate;
-            arrowObjWhileHolding.SetActive(true);
+    // public void DefaultArrow()
+    // {
+    //     if (Input.GetKey(fireButton) && _charge < chargeMax)
+    //     {
+    //         animator.Play("BowHolding");
+    //         _charge += Time.deltaTime * chargeRate;
+    //         arrowObjWhileHolding.SetActive(true);
 
-        }
+    //     }
 
-        if (Input.GetKeyUp(fireButton))
-        {
-            animator.Play("BowRelease");
+    //     if (Input.GetKeyUp(fireButton))
+    //     {
+    //         animator.Play("BowRelease");
 
-            arrowObjWhileHolding.SetActive(false);
-            GameObject arrow = Instantiate(arrowObject, spawn.position, Quaternion.identity);
-            Debug.Log(_charge.ToString());
+    //         arrowObjWhileHolding.SetActive(false);
+    //         GameObject arrow = Instantiate(arrowObject, spawn.position, Quaternion.identity);
+    //         Debug.Log(_charge.ToString());
 
-            if (_charge <= 20)
-            {
+    //         if (_charge <= 20)
+    //         {
 
-                arrow.transform.LookAt(arrowLookAtPos25);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-                _charge = 0;
-                // Destroy(arrow,2f);
+    //             arrow.transform.LookAt(arrowLookAtPos25);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             _charge = 0;
+    //             // Destroy(arrow,2f);
 
-                Debug.Log("20%");
-
-
-
-            }
-            else if (_charge > 20 && _charge <= 40)
-            {
-
-                arrow.transform.LookAt(arrowLookAtPos50);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-
-                _charge = 0;
-                // Destroy(arrow,2f);
-
-                Debug.Log("50%");
-            }
-            else if (_charge > 40 && _charge <= 80)
-            {
-                arrow.transform.LookAt(arrowLookAtPos75);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-
-                _charge = 0;
-                // Destroy(arrow,2f);
-
-            }
-            else if (_charge > 80)
-            {
-                arrow.transform.LookAt(orientRef);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-                //arrow.transform.Rotate(-_charge * 15 * Time.deltaTime, 0f, 0f, Space.Self);
-                _charge = 0;
-                // Destroy(arrow,2f);
-
-            }
-        }
-    }
-
-    public void BompArrow()
-    {
-        if (Input.GetKey(fireButton) && _charge < chargeMax)
-        {
-            animator.Play("BowHolding");
-            _charge += Time.deltaTime * chargeRate;
-            bompArrowObjWhileHolding.SetActive(true);
-
-        }
-
-        if (Input.GetKeyUp(fireButton))
-        {
-            PlayerStats.Money -= 100;
-
-            animator.Play("BowRelease");
-
-            bompArrowObjWhileHolding.SetActive(false);
-
-
-            GameObject arrow = Instantiate(bompArrowObject, spawn.position, Quaternion.identity);
-            Debug.Log(_charge.ToString());
-
-            if (_charge <= 20)
-            {
-
-                arrow.transform.LookAt(arrowLookAtPos25);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-                _charge = 0;
-                Destroy(arrow,2f);
-
-                Debug.Log("20%");
+    //             Debug.Log("20%");
 
 
 
-            }
-            else if (_charge > 20 && _charge <= 40)
-            {
+    //         }
+    //         else if (_charge > 20 && _charge <= 40)
+    //         {
 
-                arrow.transform.LookAt(arrowLookAtPos50);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             arrow.transform.LookAt(arrowLookAtPos50);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
 
-                _charge = 0;
-                Destroy(arrow,2f);
+    //             _charge = 0;
+    //             // Destroy(arrow,2f);
 
-                Debug.Log("50%");
-            }
-            else if (_charge > 40 && _charge <= 80)
-            {
-                arrow.transform.LookAt(arrowLookAtPos75);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             Debug.Log("50%");
+    //         }
+    //         else if (_charge > 40 && _charge <= 80)
+    //         {
+    //             arrow.transform.LookAt(arrowLookAtPos75);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
 
-                _charge = 0;
-                Destroy(arrow,2f);
+    //             _charge = 0;
+    //             // Destroy(arrow,2f);
 
-            }
-            else if (_charge > 80)
-            {
-                arrow.transform.LookAt(orientRef);
-                arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
-                //arrow.transform.Rotate(-_charge * 15 * Time.deltaTime, 0f, 0f, Space.Self);
-                _charge = 0;
-                Destroy(arrow,2f);
+    //         }
+    //         else if (_charge > 80)
+    //         {
+    //             arrow.transform.LookAt(orientRef);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             //arrow.transform.Rotate(-_charge * 15 * Time.deltaTime, 0f, 0f, Space.Self);
+    //             _charge = 0;
+    //             // Destroy(arrow,2f);
 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
+
+    // public void BompArrow()
+    // {
+    //     if (Input.GetKey(fireButton) && _charge < chargeMax)
+    //     {
+    //         animator.Play("BowHolding");
+    //         _charge += Time.deltaTime * chargeRate;
+    //         bompArrowObjWhileHolding.SetActive(true);
+
+    //     }
+
+    //     if (Input.GetKeyUp(fireButton))
+    //     {
+    //         PlayerStats.Money -= 100;
+
+    //         animator.Play("BowRelease");
+
+    //         bompArrowObjWhileHolding.SetActive(false);
+
+
+    //         GameObject arrow = Instantiate(bompArrowObject, spawn.position, Quaternion.identity);
+    //         Debug.Log(_charge.ToString());
+
+    //         if (_charge <= 20)
+    //         {
+
+    //             arrow.transform.LookAt(arrowLookAtPos25);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             _charge = 0;
+    //             Destroy(arrow,2f);
+
+    //             Debug.Log("20%");
+
+
+
+    //         }
+    //         else if (_charge > 20 && _charge <= 40)
+    //         {
+
+    //             arrow.transform.LookAt(arrowLookAtPos50);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+
+    //             _charge = 0;
+    //             Destroy(arrow,2f);
+
+    //             Debug.Log("50%");
+    //         }
+    //         else if (_charge > 40 && _charge <= 80)
+    //         {
+    //             arrow.transform.LookAt(arrowLookAtPos75);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+
+    //             _charge = 0;
+    //             Destroy(arrow,2f);
+
+    //         }
+    //         else if (_charge > 80)
+    //         {
+    //             arrow.transform.LookAt(orientRef);
+    //             arrow.GetComponent<Rigidbody>().AddForce(spawn.forward * _charge, ForceMode.Impulse);
+    //             //arrow.transform.Rotate(-_charge * 15 * Time.deltaTime, 0f, 0f, Space.Self);
+    //             _charge = 0;
+    //             Destroy(arrow,2f);
+
+    //         }
+    //     }
+    // }
 }
