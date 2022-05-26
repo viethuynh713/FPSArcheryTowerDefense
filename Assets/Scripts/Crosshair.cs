8ef8163eaceb0b539fbe127ed8b99d98b1f2dcbe
elboywithgear <77282469+elboywithgear@users.noreply.github.com>
2022-05-26 17:05:03 +0700

    Crosshair, etc...

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    private RectTransform crosshair;
+    public Rigidbody rigi;
+
+    public float restingSize;
+    public float maxSize;
+    public float speed;
+    public float currentSize;
+
+    public bool isMoving;
+
+    private void Start()
+    {
+        crosshair = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            currentSize = Mathf.Lerp(currentSize, maxSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            currentSize = Mathf.Lerp(currentSize, restingSize, Time.deltaTime * speed);
+        }
+
+        crosshair.sizeDelta = new Vector2(currentSize, currentSize);
+
+        
+
+        if(
+            Input.GetAxis("Horizontal") != 0 ||
+            Input.GetAxis("Vertical") != 0)
+            {
+                isMoving = true;
+            }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    
+}
