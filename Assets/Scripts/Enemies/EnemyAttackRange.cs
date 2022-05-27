a2cbea48b988f844d81b3da9fb0178c8e91888cc
elboywithgear <77282469+elboywithgear@users.noreply.github.com>
2022-05-27 10:28:13 +0700

    Ranged Enemy Fixed

--- Assets/Scripts/Enemies/EnemyAttackRange.cs
+++ Assets/Scripts/Enemies/EnemyAttackRange.cs
@@ -6,11 +6,14 @@ public class EnemyAttackRange : EnemyAttack
 {
     public GameObject enemyBullet;
 
+    public GameObject lookAtBase;
+
     public float bulletForce;
 
     void Start()
     {
         base.Start();
+        lookAtBase = GameObject.FindGameObjectWithTag("Castle");
     }
 
     void Update()
@@ -22,25 +25,38 @@ public class EnemyAttackRange : EnemyAttack
         
         if (Vector3.Distance(transform.position, GetComponent<EnemyMovement>().targetWp.position) <= distanceToTarget && canAtk == false && isAttacking == false)
         {
+
+
+            transform.LookAt(lookAtBase.transform);
+
             canAtk = true;
 
             agent.isStopped = true;
 
             agent.velocity = Vector3.zero;
         }
-        
+
+        if (Vector3.Distance(transform.position, GetComponent<EnemyMovement>().targetWp.position) > distanceToTarget)
+        {
+            // Debug.Log("Go to Base");
+            agent.isStopped = false;
+        }
+
 
     }
     public override void Attack()
     {
+        Vector3 direction = attackPos.transform.position - transform.position;
 
         anim.Play("RangedEnemyAttack");
 
         GameObject eBullet = Instantiate(enemyBullet,attackPos.transform.position, Quaternion.identity);
 
-        eBullet.GetComponent<Rigidbody>().AddForce(-attackPos.transform.position * bulletForce, ForceMode.Impulse);
+        eBullet.GetComponent<Rigidbody>().AddForce(direction * bulletForce, ForceMode.Impulse);
 
         Destroy(eBullet, 3);
+
+
     
     }
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -67,7 +67,6 @@ public class WaveSpawner : MonoBehaviour
             {
                 SpawnEnemy(waves[waveIndex].spawnEnemy[i].enemy);
                 yield return new WaitForSeconds(1f);
-
             }
         }
         waveIndex++;
